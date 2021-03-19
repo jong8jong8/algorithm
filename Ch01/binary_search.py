5c2492c0c93284ec7306d7c12c7b60a254cdc5bb
jong8jong8 <jong8jong8@gmail.com>
2021-03-19 14:19:33 +0900

    my algorithm repo.

---
+++ Ch01/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2  # int( (low + high) / 2 )
+        guess = list[mid]
+        if  guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return None
+
+
+my_list = [1, 3, 5, 7, 9] # the sorted list (in the increasing order)
+
+print(binary_search(my_list, 3)) # answer: 1
+print(binary_search(my_list, -1)) # answer: None
