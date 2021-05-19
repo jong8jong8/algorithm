b39e3195ab5c84aa5a69a2e14497136b90dcd2a0
jong8jong8 <jong8jong8@gmail.com>
2021-05-19 10:40:35 +0900

    2021-05-19

--- pythonds3/Ch01/fraction.py
+++ pythonds3/Ch01/fraction.py
@@ -16,14 +16,18 @@ class Fraction:
         self.num = top
         self.den = bottom
     def show(self):
-        print(f"{self.num}/{self.den}")
+        print(f"{self.num:d}/{self.den:d}")
     def __str__(self):
-        return f"{self.num}/{self.den}"
+        return f"{self.num:d}/{self.den:d}"
     def __add__(self, other_fraction):
         new_num = self.num * other_fraction.den + self.den * other_fraction.num
         new_den = self.den * other_fraction.den
         common = gcd(new_num, new_den)
         return Fraction(new_num // common, new_den // common)
+    def __eq__(self, other_fraction):
+        first_num = self.num * other_fraction.den
+        second_num = other_fraction.num * self.den
+        return first_num == second_num
 
 
 
@@ -58,7 +62,15 @@ class Fraction:
 # print(f1.__eq__(f2))  # shallow equality
 # print(f1 == f2)  
 
-f1 = Fraction(3, 5)
-f2 = Fraction(3, 5)
-print(f1.__eq__(f2))  # deep equality (NotImplemented by default)
-print(f1 == f2)  
+# f1 = Fraction(3, 5)
+# f2 = Fraction(3, 5)
+# print(f1.__eq__(f2))  # deep equality (NotImplemented by default)
+# print(f1 == f2)  
+
+
+x = Fraction(1, 2)
+x.show()
+y = Fraction(2, 4)
+print(y)
+print(x + y)
+print(x == y)
