853dadb394f8aec0ab60bdcff65485dcd179a82b
jong8jong8 <jong8jong8@gmail.com>
2021-05-21 19:43:02 +0900

    2021-05-21

--- pythonds3/Ch01/logic_gate.py
+++ pythonds3/Ch01/logic_gate.py
@@ -14,9 +14,16 @@ class BinaryGate(LogicGate):
         self.pin_a = None
         self.pin_b = None
     def get_pin_a(self):
-        return int(input(f"Enter pin A input (0 or 1) for gate {self.get_label()}: "))
+        if self.pin_a == None:
+            return int(input(f"Enter pin A input (0 or 1) for gate {self.get_label()}: "))
+        else:
+            return self.pin_a.get_from().get_output()
     def get_pin_b(self):
-        return int(input(f"Enter pin B input (0 or 1) for gate {self.get_label()}: "))
+        if self.pin_b == None:
+            return int(input(f"Enter pin B input (0 or 1) for gate {self.get_label()}: "))
+        else:
+            return self.pin_b.get_from().get_output()
+
     def set_next_pin(self, source):
         if self.pin_a == None:
             self.pin_a = source
@@ -24,14 +31,7 @@ class BinaryGate(LogicGate):
             if self.pin_b == None:
                 self.pin_b = source
             else:
-                raise RuntimeError("Error: NO EMPTY PINS")
-
-class UnaryGate(LogicGate):
-    def __init__(self, lbl):
-        LogicGate.__init__(self, lbl)
-        self.pin = None
-    def get_pin(self):
-        return int(input(f"Enter pin input (0 or 1) for gate {self.get_label()}: "))
+                raise RuntimeError("Cannot Connect: NO EMPTY PINS on this gate")
 
 class AndGate(BinaryGate):
     def __init__(self, lbl):
@@ -55,6 +55,23 @@ class OrGate(BinaryGate):
         else:
             return 0
 
+class UnaryGate(LogicGate):
+    def __init__(self, lbl):
+        LogicGate.__init__(self, lbl)
+        self.pin = None
+    def get_pin(self):
+        if self.pin == None:
+            return int(input(f"Enter pin input (0 or 1) for gate {self.get_label()}: "))
+        else:
+            return self.pin.get_from().get_output()
+    def set_next_pin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+
 class NotGate(UnaryGate):
     def __init__(self, lbl):
         UnaryGate.__init__(self, lbl)
@@ -76,11 +93,16 @@ class Connector:
         return self.to_gate
 
 
-g1 = AndGate("G1")
-print(g1.get_output())
+def main():
+    g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
 
-# g2 = OrGate("G2")
-# print(g2.get_output())
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
 
-# g3 = NotGate("G3")
-# print(g3.get_output())
+    print(g4.get_output())
+
+main()
